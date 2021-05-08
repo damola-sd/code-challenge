0bf9ef9b9748a4f977b756e093fd990d167b3a00
Damola Adewunmi <damolasd@gmail.com>
2021-05-08 19:04:07 +0100

    completed second task. Calculatezone function returns appropriate heart zone

--- helpers/calculateZone.js
+++ helpers/calculateZone.js
@@ -1,4 +1,22 @@
+const zones = require('./zones');
 exports.calculateZone = (resting, current) => {
-  const zone = '??';
-  return `Current HR: ${current} - Resting HR: ${resting} - Zone: ${zone}`;
+  const maximum = 180;
+  const percentageOfMaxHR = (current/maximum) * 100;
+  if(percentageOfMaxHR <= 100 && percentageOfMaxHR >= 50) {
+    const zone = zones.zones.find(function (hrZone) {
+      if (percentageOfMaxHR >= hrZone.min_percent && percentageOfMaxHR <= hrZone.max_percent) {
+        return hrZone;
+      }
+    }
+    );
+    
+    return `Current HR: ${current} - Resting HR: ${resting} - Zone: ${zone.zone}`;
+  } else if(percentageOfMaxHR < 50) {
+    return `You are not exercising hard enough. Do more`;
+
+  }else {
+    return `Slow down, you could injure yourself`;
+
+  }
+  
 };
--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const cors = require('cors');
 const v1 = require('./routes/api/v1/');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use('/api', v1());
-app.use(express.json());
+
 
 const port = 5005;
 app.listen(port, () => console.log(`Server running on port ${port}`));
