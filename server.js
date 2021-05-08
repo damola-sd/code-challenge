c3a42098725b2eb2ec2b027720589580e49f4d02
Damola Adewunmi <damolasd@gmail.com>
2021-05-08 17:26:35 +0100

    Added a body parser from express

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(cors());
 app.use('/api', v1());
+app.use(express.json());
 
 const port = 5005;
 app.listen(port, () => console.log(`Server running on port ${port}`));
